da2f5475d195370b9a98d4f55bcb80aa8f13a999
Melinda S <roseofgold@gmail.com>
2019-03-21 20:28:04 -0700

    legibility formatting

--- index.php
+++ index.php
@@ -1,25 +1,29 @@
-<?php include 'inc/header.php'; ?>
+<?php
+include 'inc/header.php';
+include "functions.php";
+?>
 
-        <section>
-            <div class="container">
-                <div class="entry-list">
-                    <article>
-                        <h2><a href="detail.html">The best day I’ve ever had</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_2.html">The absolute worst day I’ve ever had</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_3.html">That time at the mall</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_4.html">Dude, where’s my car?</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                </div>
-            </div>
-        </section>
-        <?php include 'inc/footer.php'; ?>
+<section>
+    <div class="container">
+        <div class="entry-list">
+            <article>
+                <h2><a href="detail.html">The best day I’ve ever had</a></h2>
+                <time datetime="2016-01-31">January 31, 2016</time>
+            </article>
+            <article>
+                <h2><a href="detail_2.html">The absolute worst day I’ve ever had</a></h2>
+                <time datetime="2016-01-31">January 31, 2016</time>
+            </article>
+            <article>
+                <h2><a href="detail_3.html">That time at the mall</a></h2>
+                <time datetime="2016-01-31">January 31, 2016</time>
+            </article>
+            <article>
+                <h2><a href="detail_4.html">Dude, where’s my car?</a></h2>
+                <time datetime="2016-01-31">January 31, 2016</time>
+            </article>
+        </div>
+    </div>
+</section>
+
+<?php include 'inc/footer.php'; ?>
