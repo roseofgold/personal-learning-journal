8b44d5acc288a4feac4bc1933515d24acf163213
Melinda S <roseofgold@gmail.com>
2019-03-21 19:44:06 -0700

    initial sql statements for get, add, edit, and delete functions

--- inc/functions.php
+++ inc/functions.php
@@ -1,11 +1,45 @@
-funtion addEntry(){
-
+<?php
+funtion getEntry(){
+  try{
+    $results = $db->query("SELECT * FROM entries");
+  } catch (Exception $e){
+    echo "Unable to retrieve results.";
+    exit;
+  }
 }
 
-function editEntry(){
-
+funtion addEntry($title,$date,$time_spent,$learned,$resources){
+  try{
+    $results = $db->query("
+      INSERT INTO entries
+      VALUES (NULL,$title,$date,$time_spent,$learned,$resources)
+    ");
+  } catch (Exception $e){
+    echo "Unable to retrieve results.";
+    exit;
+  }
 }
 
-function deleteEntry(){
-  
+function editEntry($title,$date,$time_spent,$learned,$resources,$id){
+  try{
+    $results = $db->query("
+      UPDATE entries
+      SET title = $title, date = $date, time_spent = $time_spent, learned = $learned, resources = $resources
+      WHERE id = $id
+    ");
+  } catch (Exception $e){
+    echo "Unable to retrieve results.";
+    exit;
+  }
 }
+
+function deleteEntry($id){
+  try{
+    $results = $db->query("DELETE FROM entries WHERE id=$id");
+  } catch (Exception $e){
+    echo "Unable to retrieve results.";
+    exit;
+  }
+}
+
+?>
