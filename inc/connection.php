f301e7625b9847aca76c02bc7328e8cbf05e0940
Melinda S <roseofgold@gmail.com>
2019-03-21 19:06:03 -0700

    Set attribute so all exceptions are seen

--- inc/connection.php
+++ inc/connection.php
@@ -2,6 +2,7 @@
 // connect to the database
 try{
     $db = new PDO("sqlite:".__DIR__."/journal.db");
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e){
   echo "Unable to connect: ";
   echo $e->getMessage();
