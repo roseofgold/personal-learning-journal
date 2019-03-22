b84189114a603c8ea656ab1dba64de5791443669
Melinda S <roseofgold@gmail.com>
2019-03-21 21:36:59 -0700

    insert the displayShortEntries function

--- index.php
+++ index.php
@@ -1,27 +1,12 @@
 <?php
 include 'inc/header.php';
-include "functions.php";
+include "inc/functions.php";
 ?>
 
 <section>
     <div class="container">
         <div class="entry-list">
-            <article>
-                <h2><a href="detail.html">The best day I’ve ever had</a></h2>
-                <time datetime="2016-01-31">January 31, 2016</time>
-            </article>
-            <article>
-                <h2><a href="detail_2.html">The absolute worst day I’ve ever had</a></h2>
-                <time datetime="2016-01-31">January 31, 2016</time>
-            </article>
-            <article>
-                <h2><a href="detail_3.html">That time at the mall</a></h2>
-                <time datetime="2016-01-31">January 31, 2016</time>
-            </article>
-            <article>
-                <h2><a href="detail_4.html">Dude, where’s my car?</a></h2>
-                <time datetime="2016-01-31">January 31, 2016</time>
-            </article>
+          <?php displayShortEntries(); ?>
         </div>
     </div>
 </section>
