d0c34117c557a7628289ab8db334f04e9fa2f8e7
Melinda S <roseofgold@gmail.com>
2019-03-23 21:03:41 -0700

    update addEntry function

--- inc/functions.php
+++ inc/functions.php
@@ -44,13 +44,18 @@ function getDetailedEntry($id){
 function addEntry($title,$date,$time_spent,$learned,$resources){
   include("connection.php");
 
+  $sql = 'INSERT INTO entries (title,date,time_spent,learned,resources) VALUES (?, ?, ?, ?, ?)';
+
   try{
-    $results = $db->query("
-      INSERT INTO entries
-      VALUES (NULL,$title,$date,$time_spent,$learned,$resources)
-    ");
+    $results = $db->prepare($sql);
+    $results->bindValue(1,$title,PDO::PARAM_STR);
+    $results->bindValue(2,$date,PDO::PARAM_STR);
+    $results->bindValue(3,$time_spent,PDO::PARAM_INT);
+    $results->bindValue(4,$learned,PDO::PARAM_STR);
+    $results->bindValue(5,$resources,PDO::PARAM_STR);
+    $results->execute();
   } catch (Exception $e){
-    echo "Unable to add entry.";
+    echo "Unable to add entry: " . $e->getMessage();
     exit;
   }
 }
