97ea335c41e98eda67ef1bfc9721aa1a45ca9172
Melinda S <roseofgold@gmail.com>
2019-03-23 20:20:53 -0700

    Add error messaging

--- new.php
+++ new.php
@@ -1,6 +1,8 @@
 <?php
+ini_set("display_errors", "on");
 include 'inc/header.php';
 include "inc/functions.php";
+
 if(!empty($_POST)){
   $title = trim(filter_input(INPUT_POST,'title',FILTER_SANITIZE_STRING));
   $date = trim(filter_input(INPUT_POST,'date',FILTER_SANITIZE_STRING));
@@ -8,7 +10,16 @@ if(!empty($_POST)){
   $whatLearned = trim(filter_input(INPUT_POST,'whatILearned',FILTER_SANITIZE_STRING));
   $resources = trim(filter_input(INPUT_POST,'ResourcesToRemember',FILTER_SANITIZE_STRING));
 
-  $dateMatch = explode('/',$date);
+  if (empty($title) || empty($date) || empty($timeSpent) || empty($whatLearned) || empty($resources)) {
+      $error_message = 'Please fill in the required fields: Title, Date, Time Spent, What I Learned, Resources To Remember';
+  } else{
+    if(add_entry($title,$date,$timeSpent,$whatLearned,$resources)){
+      header('Location: detail.php');
+      exit;
+    } else {
+      $error_message = 'Entry was not entered';
+    }
+  }
 }
 ?>
 
@@ -16,6 +27,11 @@ if(!empty($_POST)){
             <div class="container">
                 <div class="new-entry">
                     <h2>New Entry</h2>
+                    <?php
+                    if (isset($error_message)){
+                      echo "<p class='message'>$error_message</p>";
+                    }
+                    ?>
                     <form method="post" action="new.php">
                         <label for="title"> Title</label>
                         <input id="title" type="text" name="title" value="<?php echo htmlspecialchars($title); ?>"><br>
